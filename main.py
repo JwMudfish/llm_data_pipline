477b6f0346a869412563a064d540a582684bc1b5
jwmudfish <jj61183@gmail.com>
2024-04-16 19:26:51 +0900

    함수변경

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import json
 
-def read_json_file(file_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
+
+def read_file(file_path):
+    if file_path.endswith('.json'):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+    elif file_path.endswith('.txt'):
+        with open(file_path, 'r') as file:
+            data = file.read()
+    else:
+        raise ValueError("Unsupported file format")
     return data
 
-def read_txt_file(file_path):
-    with open(file_path, 'r') as file:
-        data = file.read()
-    return data
 
 
 if __name__ == '__main__':
     data_folder = './data/'
 
     novel_name = 'apple'
-    result_dic = read_json_file(data_folder + f'{novel_name}_result.json')
+    result_dic = read_file(data_folder + f'{novel_name}_result.json')
     
     for chapter in result_dic:
         print(chapter)
-        # print(result_dic[chapter])
+        
