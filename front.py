36f1b946a4ff87637a7f50620cb621022b3142d7
jwmudfish <jj61183@gmail.com>
2024-04-18 17:47:28 +0900

    streamlit 코드 추가

---
+++ front.py
@@ -0,0 +1,11 @@
+import streamlit as st
+# from streamlit.web.cli import main
+
+def main_1():
+    st.title("My Streamlit App")
+    # Add your Streamlit code here
+
+    
+
+if __name__ == "__main__":
+    main_1()
